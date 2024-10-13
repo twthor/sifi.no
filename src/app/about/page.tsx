1fe0d7249522a51aa2102231d492e661a5d32482
Tobias With Thorsen <92455258+twthor@users.noreply.github.com>
2024-10-13 19:35:42 +0200

    En første versjon av Navbar+about us er implementert + en SIFI blåfarge lagt til i tailwind config (#6)

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,48 @@
+import Image from 'next/image';
+
 const About = () => {
-  return <div> SIFI ER BEST</div>;
+  return (
+    <div className="min-h-screen bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100 p-8">
+      <div className="max-w-4xl mx-auto">
+        <h1 className="text-4xl font-bold mb-8 text-center">Om oss</h1>
+        <div className="space-y-8">
+          <section className="flex flex-col md:flex-row items-center">
+            <div>
+              <h2 className="text-2xl font-semibold mb-2"></h2>
+              <p>Sikkerhet på IFI</p>
+            </div>
+          </section>
+          <section className="flex flex-col md:flex-row items-center">
+            <div>
+              <h2 className="text-2xl font-semibold mb-2">Ta kontakt</h2>
+              <p>sikkerhet@ifi.no</p>
+            </div>
+          </section>
+          <section className="flex flex-col items-center">
+            <div>
+              <h2 className="text-2xl font-semibold mb-2">Hovedstyret</h2>
+              <p>SIFI sitt hovedstyre består av</p>
+            </div>
+            <div className="flex-shrink-0 mb-4 md:mb-0 md:mr-6">
+              <Image
+                src="/images/logo_liten_utenbak_white.png"
+                alt="Our Team"
+                width={100}
+                height={50}
+                className="w-full md:w-48 rounded-lg shadow-lg"
+              ></Image>
+            </div>
+          </section>
+          <section className="flex flex-col md:flex-row items-center">
+            <div>
+              <h2 className="text-2xl font-semibold mb-2">Interne</h2>
+              <p>Takk til alle interne!</p>
+            </div>
+          </section>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,17 +1,36 @@
 import React from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 
 const Navbar = () => {
   return (
-    <div className="flex flex-col md:flex-row">
-      <Image
-        src={'/images/logo_full_utenbak.png'}
-        alt="logo"
-        width={200}
-        height={100}
-      ></Image>
-
-      <p>Navbar incoming</p>
+    <div className="flex flex-col md:flex-row items-center justify-between md:justify-items-end p-4 md:p-8 dark:bg-gray-900">
+      <div className="flex-shrink-0">
+        <picture className="w-40 md:w-auto h-40 md:h-auto">
+          <source
+            srcSet={'/images/logo_full_utenbak_hvitskrift.png'}
+            media="(prefers-color-scheme: dark)"
+          />
+          <Image
+            src={'/images/logo_full_utenbak.png'}
+            alt="logo"
+            width={200}
+            height={100}
+            className="object-contain"
+          ></Image>
+        </picture>
+      </div>
+      <div className='flex flex-row justify-between space-x-4 mt-4 md:mt-0 text-center md:text-left"'>
+        <div className="text-gray-900 dark:text-gray-100 mt-4 md:mt-0 text-lg md:text-xl font-semibold text-center md:text-left">
+          <Link href="/">Hjem</Link>
+        </div>
+        <div className="text-gray-900 dark:text-gray-100 mt-4 md:mt-0 text-lg md:text-xl font-semibold text-center md:text-left">
+          <Link href="/arrangementer">Arrangementer</Link>
+        </div>
+        <div className="text-gray-900 dark:text-gray-100 mt-4 md:mt-0 text-lg md:text-xl font-semibold text-center md:text-left">
+          <Link href="/about">Om oss</Link>
+        </div>
+      </div>
     </div>
   );
 };
