f7d8109c80dda2f900eca81154a2c0e8d90a4708
Tobias With Thorsen <92455258+twthor@users.noreply.github.com>
2024-10-24 11:19:34 +0200

    Updated favicon to be SIFI logo (#16)
    * Footer added, adjusted colors to work with added darkmode
    
    * footer background fixed
    
    * removed unused element
    
    * Updated content for about-us, 3 latest events now visible in /arrangementer, otherwise other css adjustments
    
    * removed unused import from arrangementer/page.tsx
    
    removed unused import from arrangementer/page.tsx
    
    * Included interface for Post
    
    * Updated favicon to be SIFI logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,24 @@ const geistMono = localFont({
 export const metadata: Metadata = {
   title: 'SIFI',
   description: 'Linjeforening for informasjonssikkerhet på UiO',
+  icons: {
+    apple: '/apple-touch-icon.png',
+    icon: [
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/icon.ico', type: 'image/x-icon' }, // fallback icon for older browsers
+      {
+        url: '/android-chrome-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        url: '/android-chrome-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
