0ca266d84c6951647212d06f8e415a4b88b670e5
Julia Vister <92576942+Juliavister@users.noreply.github.com>
2025-05-13 13:52:36 +0200

    legger til styrebilder (#35)
    LGTM

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,51 @@
+import Image from 'next/image';
+const boardMembers = [
+  {
+    name: "Pernille Vannebo",
+    role: "Leder",
+    image: "/images/Styret/PernilleLeder.jpeg",
+  },
+  {
+    name: "Tobias With Thorsen",
+    role: "Nestleder",
+    image: "/images/Styret/TobiasNestleder.jpeg",
+  },
+  {
+    name: "Vegard Otterlei",
+    role: "Økonomiansvarlig",
+    image: "/images/Styret/VegardØkonomi.jpeg",
+  },
+  {
+    name: "Magnus Økstad",
+    role: "Bedriftskontakt",
+    image: "/images/Styret/MagnusBedrift.jpeg",
+  },
+  {
+    name: "Julia Vister",
+    role: "PR",
+    image: "/images/Styret/JuliaPR.jpeg",
+  },
+  {
+    name: "Adrian Skansen",
+    role: "Arrangementansvarlig",
+    image: "/images/Styret/AdrianArrangement.jpeg",
+  },
+  {
+    name: "Ahmed Abdulahi Ahmed",
+    role: "Styremedlem",
+    image: "/images/Styret/AhmedStyremedlem.jpeg",
+  },
+  {
+    name: "Emil Johannessen",
+    role: "Styremedlem",
+    image: "/images/Styret/EmilStyremedlem.jpeg",
+  },
+  {
+    name: "Sindre Vikre",
+    role: "Styremedlem",
+    image: "/images/Styret/SindreStyremedlem.jpeg",
+  },
+];
 const About = () => {
   return (
     <div className="min-h-screen dark:bg-gray-900 text-gray-900 dark:text-gray-100 p-8">
@@ -34,8 +82,40 @@ const About = () => {
                 linjeforeningen, styre økonomien og sørge for at foreningens
                 formål blir møtt.
               </p>
-              <p className="py-2">SIFI sitt hovedstyre består av:</p>
-              <ul className="list-inside list-disc pb-8">
+              <div className="w-full mt-6 aspect-[16/9]  relative">
+              <Image
+                src="/images/Styret/Styret25.jpeg"
+                alt="Styret 2025"
+                width={1920}
+                height={1080}
+                className="w-full rounded-lg shadow-lg"
+              ></Image>
+              </div>
+              {/*<p className="py-2">SIFI sitt hovedstyre består av:</p> */}
+
+              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 mt-6">
+              {boardMembers.map((member) => (
+                <div
+                  key={member.name}
+                  className="flex flex-col items-center text-center bg-white dark:bg-gray-800 p-4 rounded-lg shadow-md"
+                 >
+                  <div className="w-48 h-48 relative mb-4">
+                    <Image
+                      src={member.image}
+                      alt={member.name}
+                      fill
+                      sizes="(max-width: 768px) 100vw, 200px"
+                      className="rounded-full object-cover"
+                    />
+                  </div>
+                  <p className="font-semibold">{member.name}</p>
+                  <p className="text-sm text-gray-600 dark:text-gray-300">
+                    {member.role}
+                  </p>
+                </div>
+              ))}
+          </div>
+              {/*<ul className="list-inside list-disc pb-8">
                 <li>Leder: Pernille Vannebo</li>
                 <li>Nestleder: Tobias With Thorsen</li>
                 <li>Økonomiansvarlig: Vegard Otterlei</li>
@@ -45,8 +125,8 @@ const About = () => {
                 <li>Styremedlem: Ahmed Abdulahi Ahmed</li>
                 <li>Styremedlem: Emil Johannessen</li>
                 <li>Styremedlem: Sindre Vikre</li>
-              </ul>
-            </div>
+              </ul>*/}
+            </div> 
             <div className="flex-shrink-0 mb-4 md:mb-0 md:mr-6">
               {/* Legg inn bildet av styret her! */}
               {/* <Image
